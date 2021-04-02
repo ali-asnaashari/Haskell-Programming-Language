348bf39def71da78a2045ae682a6a372ca830fd3
ali-asnaashari <ali.asnaashari2000@gmail.com>
2021-04-02 12:39:55 +0430

    Email

--- sectionOne.hs
+++ sectionOne.hs
@@ -1,3 +1,18 @@
--- firstProgram
+{-
+
+-- First Program
 main = do
     print "hello world"
+
+-}
+
+-- Second Program
+main :: IO()
+main = do
+   print "Who is the email for?"
+   recipient <- getLine
+   print "What is the Title?"
+   title <- getLine
+   print "Who is the Author?"
+   author <- getLine
+   print ("Dear " ++ recipient ++ ",\n " ++ "Thanks for buying " ++ title  ++ "\n thanks,\n " ++ author )
