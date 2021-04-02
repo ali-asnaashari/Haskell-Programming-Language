429a14bc4b5c1c91dee19cc17be11d8a3fd8486b
ali-asnaashari <ali.asnaashari2000@gmail.com>
2021-04-02 12:49:52 +0430

    Define Four Function

--- sectionOne.hs
+++ sectionOne.hs
@@ -1,13 +1,15 @@
 {-
-
 -- First Program
+
 main = do
     print "hello world"
 
 -}
 
+
+{-
 -- Second Program
-main :: IO()
+
 main = do
    print "Who is the email for?"
    recipient <- getLine
@@ -16,3 +18,27 @@ main = do
    print "Who is the Author?"
    author <- getLine
    print ("Dear " ++ recipient ++ ",\n " ++ "Thanks for buying " ++ title  ++ "\n thanks,\n " ++ author )
+
+-}
+
+
+{-
+-- Third Program
+
+-- define three Function
+
+toPart recipient = "Dear " ++ recipient ++ ",\n "
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n "
+fromPart author = "Thanks,\n " ++ author
+createEmail recipient bookTitle author = toPart recipient ++ bodyPart bookTitle ++ fromPart author
+
+main = do
+   print "Who is the email for?"
+   recipient <- getLine
+   print "What is the Title?"
+   title <- getLine
+   print "Who is the Author?"
+   author <- getLine
+   print (createEmail recipient title author)
+
+-}
